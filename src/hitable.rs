f633b4bee48a4a44fc34829b1719570791fbc0d7
Alexander Molchanov <bonext@mailbox.org>
2019-06-16 22:04:52 +0300

    Now I have a hitable list (somewhat)

--- src/hitable.rs
+++ src/hitable.rs
@@ -11,3 +11,35 @@ pub struct HitRecord {
 pub trait Hitable {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32, record: &mut HitRecord) -> bool;
 }
+
+pub struct HitableList<'a, T: Hitable> {
+    items: &'a [T]
+}
+
+impl<'a, T: Hitable> HitableList<'a, T> {
+    pub fn from(items: &'a [T]) -> HitableList<'a, T> {
+        HitableList{items}
+    }
+}
+
+impl<'a, T: Hitable> Hitable for HitableList<'a, T> {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, record: &mut HitRecord) -> bool {
+        // let's implement iteration over hitables here at first
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+        let mut temp_rec = HitRecord {
+            t: 0.0,
+            p: Vector3f::new(0.0, 0.0, 0.0),
+            normal: Vector3f::new(0.0, 0.0, 0.0)
+        };
+        for item in self.items.iter() {
+            if item.hit(r, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *record = temp_rec;
+            }
+        }
+        return hit_anything;
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,27 @@
 extern crate image;
-use std::env;
-use std::f32;
+
+use std::{env, f32};
+
 mod vector;
 mod ray;
-mod hitable;
 mod sphere;
+mod hitable;
 use vector::Vector3f;
+use ray::Ray;
 use sphere::Sphere;
-use hitable::HitRecord;
-use hitable::Hitable;
+use hitable::{HitRecord, Hitable, HitableList};
 
-fn compute_color<T: Hitable>(r: &ray::Ray, world: &[T]) -> Vector3f {
+fn compute_color<'a, T: Hitable>(r: &Ray, world: &'a HitableList<'a, T>) -> Vector3f {
     let hit_t_min = 0.0;
     let hit_t_max = std::f32::MAX;
 
-    // let's implement iteration over hitables here at first
-    let mut hit_anything = false;
-    let mut closest_so_far = hit_t_max;
-    let mut temp_rec = HitRecord {
-        t: 0.0,
-        p: Vector3f::new(0.0, 0.0, 0.0),
-        normal: Vector3f::new(0.0, 0.0, 0.0)
-    };
-    for item in world.iter() {
-        if item.hit(r, hit_t_min, closest_so_far, &mut temp_rec) {
-            hit_anything = true;
-            closest_so_far = temp_rec.t;
-            // *record = temp_rec;
-        }
-    }
+     let mut temp_rec = HitRecord {
+         t: 0.0,
+         p: Vector3f::new(0.0, 0.0, 0.0),
+         normal: Vector3f::new(0.0, 0.0, 0.0)
+     };
 
-    if hit_anything {
+    if world.hit(r, hit_t_min, hit_t_max, &mut temp_rec) {
         return (temp_rec.normal + Vector3f::new(1.0, 1.0, 1.0)) * 0.5;
     }
 
@@ -52,7 +43,7 @@ fn main() {
     let vertical = Vector3f::new(0.0, 2.0, 0.0);
     let origin = Vector3f::new(0.0, 0.0, 0.0);
 
-    let world = vec![
+    let spheres = vec![
         Sphere::new(
             Vector3f::new(0.0, 0.0, -1.0), 0.5
         ),
@@ -60,15 +51,16 @@ fn main() {
             Vector3f::new(0.0, -100.5, -1.0), 100.0
         )
     ];
+    let world = HitableList::from(&spheres[..]);
 
     let mut img = image::ImageBuffer::new(width, height);
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let u = x as f32 / width as f32;
         let v = (height - y) as f32 / height as f32;
 
-        let r = ray::Ray::new(origin, llc + horizontal * u + vertical * v);
+        let r = Ray::new(origin, llc + horizontal * u + vertical * v);
 
-        let color = compute_color(&r, &world[..]);
+        let color = compute_color(&r, &world);
 
         *pixel = image::Rgb([
             (255.99 * color.x) as u8,
