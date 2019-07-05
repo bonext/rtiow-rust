d0b4696d05724788ce0682155e0875b3839a51f4
Alexander Molchanov <bonext@mailbox.org>
2019-07-05 11:32:34 +0300

    Add metal

--- src/materials.rs
+++ src/materials.rs
@@ -8,13 +8,25 @@ pub trait Material {
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Lambertian {
-    pub attenuation: Vector3f,
+    pub albedo: Vector3f,
 }
 
 impl Material for Lambertian {
     fn scatter(&self, hit: HitRecord) -> Option<(Ray, Vector3f)> {
         let scatter_position = hit.p + hit.normal + Vector3f::random_unit();
         let scattered_ray = Ray::new(hit.p, scatter_position - hit.p);
-        return Some((scattered_ray, self.attenuation));
+        return Some((scattered_ray, self.albedo));
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Metal {
+    pub albedo: Vector3f
+}
+
+impl Material for Metal {
+    fn scatter(&self, hit: HitRecord) -> Option<(Ray, Vector3f)> {
+        let reflected = hit.in_ray.direction - hit.normal * (2.0 * hit.in_ray.direction.dot(hit.normal));
+        Some((Ray::new(hit.p, reflected), self.albedo))
     }
 }
